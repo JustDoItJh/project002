4d193772b45ea7c0373fd694879568ae9b49579d
pjh <wlsgur0217@naver.com>
2022-07-10 23:02:41 +0900

    22.07.10 작업

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,6 +1,18 @@
 $(function(){
 
-
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
     var swiper = new Swiper(".sc-collection .swiper", {
         slidesPerView:'auto',
